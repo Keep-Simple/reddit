13df88d98f41cf131a37526d17d59c02ef264715
Keep Simple <nikyasnogorodskyi@gmail.com>
2020-09-18 11:54:20 +0300

    Basic infrastructure

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import "reflect-metadata"
+import {MikroORM} from "@mikro-orm/core"
+import mikroConfig from "./mikro-orm.config"
+import express from "express"
+import {ApolloServer} from 'apollo-server-express'
+import {buildSchema} from 'type-graphql'
+import {HelloResolver} from "./resolvers/hello"
+import {PostResolver} from "./resolvers/post"
+
+const main = async () => {
+    const orm = await MikroORM.init(mikroConfig)
+    await orm.getMigrator().up()
+
+    const app = express()
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false
+        }),
+        context: () => ({ em: orm.em })
+    })
+
+    apolloServer.applyMiddleware({app})
+
+    app.listen(4000, () => console.log('server started on localhost:4000'))
+}
+main()
