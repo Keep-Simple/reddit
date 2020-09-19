6e58fc91e4821471c73cf66373f4e83e7aa5aefd
Keep Simple <nikyasnogorodskyi@gmail.com>
2020-09-19 12:11:51 +0300

    remove odd files

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,22 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  Icon,
+  List,
+  ListIcon,
+  ListItem,
+} from '@chakra-ui/core'
+
+import { Hero } from '../components/Hero'
+import { Container } from '../components/Container'
+import { Main } from '../components/Main'
+import { DarkModeSwitch } from '../components/DarkModeSwitch'
+import { CTA } from '../components/CTA'
+import { Footer } from '../components/Footer'
+
+const Index = () => (
+  <div>hello world</div>
+)
+
+export default Index
